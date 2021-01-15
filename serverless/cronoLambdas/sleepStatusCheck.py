91631b4759f74f5cae28478feebf6bd503458a0c
nestor <nestorcolt@hotmail.com>
2021-01-15 01:17:23 +0100

    update

--- serverless/cronoLambdas/sleepStatusCheck.py
+++ serverless/cronoLambdas/sleepStatusCheck.py
@@ -1,5 +1,6 @@
 from Cloud.packages.constants import constants
 from Cloud.packages.sns import sns_manager
+from Cloud.packages.Ec2 import ec2_manager
 from Cloud.packages.dynamo import modules
 from Cloud.packages import logger
 import json
@@ -20,11 +21,16 @@ def sleep_status_check_handler(event, context):
 
     for user in last_active["Items"]:
         search_blocks = user.get("search_blocks", False)
+        user_id = str(user["user_id"])
 
-        if search_blocks:
+        # Get the running instance with the user id if exists
+        instance_name = f'User-{user_id}'
+        instance = ec2_manager.get_instance_by_tag(value=instance_name)  # None = Instance doesn't not exist
+
+        if search_blocks and instance is None:
             topic_arn = sns_manager.get_topic_by_name(constants.START_SE_SNS_NAME)[0]["TopicArn"]
             sns_manager.sns_publish_to_topic(topic_arn=topic_arn,
-                                             message=json.dumps(str(user["user_id"])),
+                                             message=json.dumps(user_id),
                                              subject="SleepCheckEvent")
 
 ##############################################################################################
