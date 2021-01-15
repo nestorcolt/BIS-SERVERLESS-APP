ede6c35e11325be319bf832919efb6605c49430a
nestor <nestorcolt@hotmail.com>
2021-01-15 18:33:57 +0100

    put new block

--- serverless/capturedBlocks/updateBlocksTable.py
+++ serverless/capturedBlocks/updateBlocksTable.py
@@ -1,2 +1,19 @@
+from Cloud.packages.dynamo import modules
+import json
+
+
+##############################################################################################
+
 def lambda_handler(event, context):
-    pass
+    """
+    Clean up the table blocks from blocks older than 48 hours from the time the function is called
+    """
+    # Get the records list
+    records = event["Records"][0]
+    blocks = json.loads(records["Sns"]["Message"])["blocks"]
+
+    for block in blocks:
+        modules.put_new_block(block["user_id"], block["data"])
+        # TODO better to log this for cloud watch debbuging
+
+##############################################################################################
