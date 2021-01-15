b8acda6f8b782da92e492889d24c94985e138a1b
nestor <nestorcolt@hotmail.com>
2021-01-15 18:29:06 +0100

    crono lambda clean up blocks

--- serverless/cronoLambdas/cleanBlocksTable.py
+++ serverless/cronoLambdas/cleanBlocksTable.py
@@ -1,2 +1,12 @@
+from Cloud.packages.dynamo import modules
+
+
+##############################################################################################
+
 def lambda_handler(event, context):
-    pass
+    """
+    Clean up the table blocks from blocks older than 48 hours from the time the function is called
+    """
+    modules.cleanup_blocks_table()
+
+##############################################################################################
