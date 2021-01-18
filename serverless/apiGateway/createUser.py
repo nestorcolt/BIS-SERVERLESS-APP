969d3a011306ab525a6475aec4d9370d6307ab6f
nestor <nestorcolt@hotmail.com>
2021-01-18 22:30:57 +0100

    create user modify user

--- serverless/apiGateway/createUser.py
+++ serverless/apiGateway/createUser.py
@@ -1,19 +1,61 @@
-from Cloud.packages.dynamo import dynamo_manager
-from decimal import Decimal
-import boto3
-import json
+from Cloud.packages.dynamo import dynamo_manager, controller
+from Cloud.packages.constants import constants
+from Cloud.packages import logger
+
+LOGGER = logger.Logger(__name__)
+log = LOGGER.logger
 
 
 ##############################################################################################
+
 
 def function_handler(event, context):
+    new_headers = controller.map_request_body({}, event)
 
-    # dynamo_manager.create_item("Users", dictionary_item=event)
+    try:
+        dynamo_manager.create_item(constants.USERS_TABLE_NAME, dictionary_item=new_headers)
+    except Exception as e:
+        log.error(e)
+        return {"success": False, "message": e, "data": {}}
 
-    return {
-        "success": True,
-        "message": [],
-        "data": event
-    }
+    return {"success": True, "message": "Entry created", "data": new_headers}
+
 
 ##############################################################################################
+s = {
+    "user_id": "2",
+    "refresh_token": "",
+    "areas": [],
+    "search_blocks": 1,
+    "last_active": 0,
+    "speed": 1,
+    "minimum_price": 20.00,
+    "search_schedule": [
+        {
+            "dayOfWeek": 0,
+            "enable": 1,
+            "times": [
+                {
+                    "blockTime": 1,
+                    "start": "08:00",
+                    "end": "10:00"
+                },
+                {
+                    "blockTime": 1,
+                    "start": "08:00",
+                    "end": "10:00"
+                },
+                {
+                    "blockTime": 2,
+                    "start": "08:00",
+                    "end": "10:00"
+                },
+                {
+                    "blockTime": 2,
+                    "start": "08:00",
+                    "end": "10:00"
+                }
+            ]
+        }
+    ]
+}
--- serverless/apiGateway/updateUser.py
+++ serverless/apiGateway/updateUser.py
@@ -1,17 +1,27 @@
-from Cloud.packages.dynamo import dynamo_manager
-from decimal import Decimal
-import boto3
-import json
+from Cloud.packages.dynamo import dynamo_manager, controller
+from Cloud.packages.constants import constants
+from Cloud.packages import logger
+
+LOGGER = logger.Logger(__name__)
+log = LOGGER.logger
 
 
 ##############################################################################################
 
-def function_handler(event, context):
-        return {
-                "success": True,
-                "message": [],
-                "data": event
-        }
 
+def function_handler(event, context):
+    new_headers = controller.map_request_body({}, event)
+    user_id = new_headers.pop(constants.TABLE_PK)
+
+    try:
+        dynamo_manager.update_item(constants.USERS_TABLE_NAME,
+                                   constants.TABLE_PK,
+                                   user_id,
+                                   new_headers)
+    except Exception as e:
+        log.error(e)
+        return {"success": False, "message": e, "data": {}}
+
+    return {"success": True, "message": "Entry created", "data": new_headers}
 
 ##############################################################################################
