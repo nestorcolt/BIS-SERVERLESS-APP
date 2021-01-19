06ce2ef0e7e1282f1c4692dba82eef07753ad4e1
nestor <nestorcolt@hotmail.com>
2021-01-19 15:29:08 +0100

    definition and link api gateway + lambda format done

--- serverless/apiGateway/createUser.py
+++ serverless/apiGateway/createUser.py
@@ -1,6 +1,7 @@
 from Cloud.packages.dynamo import dynamo_manager, controller
 from Cloud.packages.constants import constants
 from Cloud.packages import logger
+import json
 
 LOGGER = logger.Logger(__name__)
 log = LOGGER.logger
@@ -10,14 +11,23 @@ log = LOGGER.logger
 
 
 def function_handler(event, context):
-    new_headers = controller.map_request_body({}, event)
+
+    new_headers = controller.map_request_body({}, json.loads(event["body"]))
+    status_code = 200
+    message = "Entry on Users table created successfully"
 
     try:
         dynamo_manager.create_item(constants.USERS_TABLE_NAME, dictionary_item=new_headers)
     except Exception as e:
         log.error(e)
-        return {"success": False, "message": e, "data": {}}
+        message = e
+        status_code = 410
 
-    return {"success": True, "message": "Entry created", "data": new_headers}
+    return {
+        "statusCode": status_code,
+        "body": json.dumps({
+            "message": message,
+        }),
+    }
 
 ##############################################################################################
