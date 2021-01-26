83d4f51c8aa3d0c64bac2b24de9b01f6880b0871
nestor <nestorcolt@hotmail.com>
2021-01-26 13:19:32 +0100

    my commit jsonsimple

--- serverless/searchEngine/flexListActiveUsers.py
+++ serverless/searchEngine/flexListActiveUsers.py
@@ -2,6 +2,7 @@ from Cloud.packages.constants import constants
 from Cloud.packages.dynamo import controller
 from Cloud.packages.sns import sns_manager
 from Cloud.packages import logger
+import simplejson
 
 LOGGER = logger.Logger(__name__)
 log = LOGGER.logger
@@ -19,12 +20,22 @@ def function_handler(event, context):
 
     for user_data in last_active["Items"]:
         search_blocks = user_data.get("search_blocks", False)
+        access_token = user_data.get("access_token", "")
 
-        if search_blocks:
-            log.debug(user_data)
-            topic_arn = sns_manager.get_topic_by_name(constants.START_SE_SNS_NAME)[0]["TopicArn"]
-            # sns_manager.sns_publish_to_topic(topic_arn=topic_arn,
-            #                                  message=user_data,
-            #                                  subject="User filters for search engine")
+        if not search_blocks:
+            continue
+
+        if len(access_token) == 0:
+            topic_arn = sns_manager.get_topic_by_name(constants.AUTHENTICATE_SE_SNS_NAME)[0]["TopicArn"]
+            sns_manager.sns_publish_to_topic(topic_arn=topic_arn,
+                                             message=simplejson.dumps(user_data, use_decimal=True),
+                                             subject="Get access token for user")
+            continue
+
+        # If passed all validations let user search
+        topic_arn = sns_manager.get_topic_by_name(constants.START_SE_SNS_NAME)[0]["TopicArn"]
+        sns_manager.sns_publish_to_topic(topic_arn=topic_arn,
+                                         message=simplejson.dumps(user_data, use_decimal=True),
+                                         subject="User filters for search engine")
 
 ##############################################################################################
