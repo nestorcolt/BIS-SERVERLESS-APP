26b05326e5b228c5e2dcbd231782bf490f0a9aca
nestor <nestorcolt@hotmail.com>
2021-01-27 14:04:20 +0100

    error to web

---
+++ serverless/apiGateway/notifyBlockToWeb.py
@@ -0,0 +1,26 @@
+from Cloud.packages.dynamo import controller
+from Cloud.packages import logger
+import json
+
+LOGGER = logger.Logger(__name__)
+log = LOGGER.logger
+
+
+##############################################################################################
+
+def lambda_handler(event, context):
+    """
+    Triggered by an SNS event will put a new entry on the blocks table with the captured blocks from the user.
+    This SNS event will be called inside of an Ec2 instance from the search engine
+    """
+
+    # Get the records list
+    block = json.loads(event["Records"][0]["Sns"]["Message"])
+
+    user = block["user_id"]
+    data = block["data"]
+
+    response = controller.send_block_to_web(user, data)
+    log.info(f"Response of request: {response.status_code}")
+
+##############################################################################################
--- serverless/apiGateway/notifyErrorToWeb.py
+++ serverless/apiGateway/notifyErrorToWeb.py
@@ -13,15 +13,9 @@ def lambda_handler(event, context):
     Triggered by an SNS event will put a new entry on the blocks table with the captured blocks from the user.
     This SNS event will be called inside of an Ec2 instance from the search engine
     """
-
     # Get the records list
-    records = event["Records"][0]
-    blocks = json.loads(records["Sns"]["Message"])["blocks"]
-
-    for block in blocks:
-        user = block["user_id"]
-        data = block["data"]
-        response = controller.send_block_to_web(user, data)
-        log.info(f"Response of request: {response.status_code}")
+    user_id = json.loads(event["Records"][0]["Sns"]["Message"])
+    response = controller.send_error_to_web(user_id)
+    log.info(f"Response of request: {response.status_code}")
 
 ##############################################################################################
