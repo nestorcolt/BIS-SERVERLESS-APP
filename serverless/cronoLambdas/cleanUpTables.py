444d75e33ca168600cf0081d9c203f5f2f09a90c
nestor <nestorcolt@hotmail.com>
2021-01-28 12:42:21 +0100

    crono lambda modified

--- serverless/cronoLambdas/cleanUpTables.py
+++ serverless/cronoLambdas/cleanUpTables.py
@@ -1,3 +1,4 @@
+from Cloud.packages.constants import constants
 from Cloud.packages.dynamo import controller
 from Cloud.packages import logger
 
@@ -12,6 +13,7 @@ def lambda_handler(event, context):
     Clean up the table blocks from blocks older than 48 hours from the time the function is called
     """
     controller.cleanup_blocks_table()
-    log.info("Entries for 'Blocks' table cleaned up successfully")
+    controller.cleanup_offers_table()
+    log.info(f"Entries over {constants.CLEANUP_BLOCKS_TIME_THRESHOLD} hours removed from tables:\n-Blocks\n-Offers")
 
 ##############################################################################################
