aaa9e1824f83e46eeb478727f5945e9ac51c677a
nestor <nestorcolt@hotmail.com>
2021-03-23 23:32:24 +0100

    lastes layer pre procesor lambda

--- serverless/searchEngine/getLatestCsharpLayer.py
+++ serverless/searchEngine/getLatestCsharpLayer.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 
 ##############################################################################################
 
@@ -6,6 +7,17 @@ TARGET_LAYER_NAME = "DotnetCloudLibrary"
 
 
 def function_handler(event, context):
+    layer_version = get_latest_layer()
+    
+    return {
+        'requestId': event['requestId'],
+        'status': 'success',
+        'fragment': layer_version
+    }
+
+
+##############################################################################################
+def get_latest_layer():
     client = boto3.client('lambda')
     layers = client.list_layers(
         CompatibleRuntime='dotnetcore3.1',
