600268210d7c399bdae927ca1dc1ebaac32775e3
nestor <nestorcolt@hotmail.com>
2021-04-07 14:25:53 +0200

    new cron event to reset stats table

--- sandbox/dynamo.py
+++ sandbox/dynamo.py
@@ -26,7 +26,7 @@ def switch_user_state(user_id, value):
     dynamo_manager.update_item("Users", "user_id", user_id, {"search_blocks": value})
 
 
-def get_offers(user_id):
+def put_fake_blocks(user_id):
     """
     """
     offers = controller.get_offers(user_id)
@@ -36,7 +36,21 @@ def get_offers(user_id):
         controller.put_new_block(user_id, data)
 
 
+def get_offers(user_id):
+    """
+    """
+    offers = controller.get_offers(user_id)
+    print(len(offers))
+    for offer in offers:
+        data = offer["data"]
+        string = simplejson.dumps(data)
+
+        if string.__contains__("RESERVED"):
+            print(data)
+            break
+
+
 ##############################################################################################
 if __name__ == '__main__':
     # function_handler()
-    get_offers("5")
+    get_offers("27")
