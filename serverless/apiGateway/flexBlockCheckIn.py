586de60ada8251f8e8514f9b8cf29fbf3081abbb
nestor <nestorcolt@hotmail.com>
2021-04-21 10:43:30 +0200

    checkin api done apparently

--- serverless/apiGateway/flexBlockCheckIn.py
+++ serverless/apiGateway/flexBlockCheckIn.py
@@ -1,26 +1,31 @@
 from Cloud.packages.controller import check_in_controller
-from Cloud.packages.controller import user_controller
 from Cloud.packages.constants import constants
-from Cloud.packages.dynamo import controller
-from Cloud.packages.sns import sns_manager
 from Cloud.packages import logger
 import json
 
 LOGGER = logger.Logger(__name__)
 log = LOGGER.logger
 
+
 ##############################################################################################
-user_id = "5"
-longitude = ""
-latitude = ""
 
 
 def function_handler(event, context):
-    # body = event.get("body")
+    body = event.get("body")
     status_code = 200
     output = None
 
-    check_in_controller.check_in_block({"user_id": user_id, "longitude": longitude, "latitude": latitude})
+    user_id = ""
+    longitude = ""
+    latitude = ""
+
+    try:
+        output = check_in_controller.check_in_block({"user_id": user_id,
+                                                     "longitude": longitude,
+                                                     "latitude": latitude})
+    except Exception as e:
+        status_code = 400
+        log.error(e)
 
     return {
         "statusCode": status_code,
