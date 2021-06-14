299e5d30928c0f68ead4a47f3ffe033e35a4512b
nestor <nestorcolt@hotmail.com>
2021-06-14 22:10:28 +0200

    upload

--- serverless/apiGateway/getBisUsers.py
+++ serverless/apiGateway/getBisUsers.py
@@ -1,13 +1,14 @@
 from Cloud.packages.aurora import controller
 import json
 
-
 ##############################################################################################
+
+conn = controller.connection_handler()
+
 
 def function_handler(event, context):
     # will query and return aurora data base to get all the users in the platform
-    bis_user_list = controller.get_bis_users()
-
+    bis_user_list = controller.get_bis_users(conn)
     return {
         "statusCode": 200,
         "body": json.dumps({
