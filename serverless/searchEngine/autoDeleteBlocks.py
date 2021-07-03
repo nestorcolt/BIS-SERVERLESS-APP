4dbe40384b0f5504a9a7c1becc997229bf24fe1e
nestor <nestorcolt@hotmail.com>
2021-07-03 20:24:32 +0200

    updated sns permissions to autoclean up table blocks

--- serverless/searchEngine/autoDeleteBlocks.py
+++ serverless/searchEngine/autoDeleteBlocks.py
@@ -1,15 +1,9 @@
 from Cloud.packages.sns import sns_manager
-from aws_lambda_powertools import Tracer
-from Cloud.packages import logger
 
-LOGGER = logger.Logger(__name__)
-log = LOGGER.logger
 
 ##############################################################################################
-tracer = Tracer()
 
 
-@tracer.capture_lambda_handler
 def function_handler(event, context):
     # send to sns event to delete all items with C# (faster)
     topic_arn = sns_manager.get_topic_by_name("DropBlocksTable")[0]["TopicArn"]
