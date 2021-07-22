7bf3ac90da01b5bae40c1e8b67d88506ea86bce8
nestor <nestorcolt@hotmail.com>
2021-07-22 11:19:58 +0200

    reset stats table get users method from stats table implemented

--- serverless/searchEngine/resetUserStats.py
+++ serverless/searchEngine/resetUserStats.py
@@ -1,19 +1,17 @@
 from Cloud.packages.constants import constants
 from Cloud.packages.dynamo import controller
-from aws_lambda_powertools import Tracer
+
 
 ##############################################################################################
-tracer = Tracer()
 
 
-@tracer.capture_lambda_handler
 def function_handler(event, context):
     """
     Get all users available un the Users table and will set the atomic counters back to zero every
     24 hours. Meaning: every 0:00:00 from every day UTC
     """
     # Get the records list
-    users = controller.get_all_users()
+    users = controller.get_all_users_in_stats_table()
 
     for user_data in users["Items"]:
         user = user_data[constants.TABLE_PK]
