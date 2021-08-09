e2e38f92b92304da4cb9b421e73b87b78435d2d8
nestorcolt <nestor.colt@gmail.com>
2021-08-09 22:11:55 +0200

    get logs api func getting user input

--- serverless/apiGateway/getSearchEngineCloudLogs.py
+++ serverless/apiGateway/getSearchEngineCloudLogs.py
@@ -1,4 +1,5 @@
 from Cloud.packages.cloudwatch import logs_manager
+from Cloud.packages.constants import constants
 from aws_lambda_powertools import Tracer
 from Cloud.packages import logger
 import json
@@ -13,10 +14,16 @@ tracer = Tracer()
 @tracer.capture_lambda_handler
 def function_handler(event, context):
     status_code = 200
+    user_id = None
     logs = []
 
+    body = event["body"]
+
+    if body is not None:
+        user_id = json.loads(body).get(constants.TABLE_PK)
+
     try:
-        logs = logs_manager.describe_search_engine_logs()
+        logs = logs_manager.describe_search_engine_logs(user_id)
     except Exception as e:
         log.error(e)
         status_code = 410
